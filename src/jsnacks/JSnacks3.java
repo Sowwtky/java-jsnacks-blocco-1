96f6daa0718c28853eeb35980d3cd9e92cd700f6
Sowwtky <miche46ale@gmail.com>
2022-06-13 11:26:14 +0200

    jsnacks3

---
+++ src/jsnacks/JSnacks3.java
@@ -0,0 +1,30 @@
+package jsnacks;
+
+import java.util.Random;
+
+public class JSnacks3 {
+
+	public static void main(String[] args) {
+		//Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una
+		//lista di cognomi, genera una falsa lista di 3 invitati.
+		
+		String[] nomi = {"Alessia", "Alex", "Valentina"};
+		String[] cognomi = {"Biagi", "Panichi", "Di Raco"};
+		String nomeCasuale;
+		String cognomeCasuale;
+		int i;
+		
+		Random ran = new Random();
+		
+		for(i = 0; i <= 2; i++) {
+			nomeCasuale = nomi[ran.nextInt(3)];
+			System.out.print(nomeCasuale + " ");
+			cognomeCasuale = cognomi[ran.nextInt(3)];
+			System.out.println(cognomeCasuale);
+		}
+		
+		
+		}
+	}
+
+
